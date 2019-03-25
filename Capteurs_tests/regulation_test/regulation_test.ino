6b833e03bc963518f79e5cd88573c852e44efc41
PaulThonney <paul.thonney@cpnv.ch>
2019-03-25 13:09:23 +0100

    Update regulation_test.ino

--- Capteurs_tests/regulation_test/regulation_test.ino
+++ Capteurs_tests/regulation_test/regulation_test.ino
@@ -1,8 +1,9 @@
 byte oldpulse1 = 0;
 double pps[5]; //pulse par secondes
 double tps[5]; // tours par secondes
-double avgSpeed[5]; // vitesse estimée en m/s
-double R = 0.02;
+double mesuredSpeed[5]; // vitesse estimée en m/s
+double R = 0.02; //Rayon de la roue
+unsigned int nbrtrous = 50; // nombre de trou dans la roue
 unsigned long currentTime[2];
 
 void setup() {
@@ -17,33 +18,38 @@ void loop() {
 
 
 
-  //Serial.println(longueure);
+
   for (byte j = 0; j < 5; j++) {
+    
     byte yolo = 0;
+    
     while (yolo < 2) {
+      
       byte pulse1 = digitalRead(2);
 
-      //Serial.println(yolo);
       if (pulse1 > oldpulse1) {
-        //Serial.println("enter if");
+     
 
         currentTime[yolo] = micros();
+        
         yolo++;
       }
       oldpulse1 = pulse1;
     }
-    //Serial.println("exit while");
+
 
 
     pps[j] = 1000000 / (currentTime[1] - currentTime[0]); // le million est pour revenir en pulse/secondes
-    // Serial.println(currentTime[1]-currentTime[0]);
-    tps[j] = pps[j] / 50;
-    avgSpeed[j] = 2 * PI * tps[j] * R;
+
+    tps[j] = pps[j] / nbrtrous; //calcul du nombre de tours/secondes
+    
+    mesuredSpeed[j] = 2 * PI * tps[j] * R; //calcul de la vitesse
 
   }
   for (int i = 0; i < 5; i++) {
-    //Serial.println(pps[i]);
-    Serial.print(avgSpeed[i]);
+
+    Serial.print(mesuredSpeed[i]); //les prints prennent trop de temps ils seront supprimé de la version définitive
+    
     Serial.println(" m/s");
   }
 }
