3e408f50c5ea41a3d23ebb4e4ab66f093a443010
PaulThonney <paul.thonney@cpnv.ch>
2019-03-26 15:00:13 +0100

    TESTDEFUSIONDESPROGRAMMES

--- Capteurs_tests/Regulation_Moteur_l_onard_modifi_/Regulation_Moteur_l_onard_modifi_.ino
+++ Capteurs_tests/Regulation_Moteur_l_onard_modifi_/Regulation_Moteur_l_onard_modifi_.ino
@@ -1,23 +1,9 @@
 #include <Wire.h>
 #include <SPI.h>
 
-#define MOTORDIRECTIONPIN 4
+
 #define MOTORSPEEDPIN 5
-#define ENCODERA 12
-#define ENCODERB 11
 #define SENSORPIN 2
-#define LEDGREEN 10
-#define LEDRED 9
-#define SWITCH 8
-
-
-
-int flagSensTrig = 0; // Sens of the encoder 0 when not specified 1 when clock wise 2 when counter clock wise
-// flag for just one turn
-bool flag = false;
-
-unsigned long encoderTimer = 0;
-unsigned long encoderDuration = 20;
 
 unsigned long timerPID = 0;
 unsigned long durationPID = 20;
@@ -34,7 +20,7 @@ unsigned long durationI2c = 500;
 // Speed for the motor
 byte motorCurrentSpeed = 0;
 
-byte motorObjectiveSpeed = 60;
+byte motorObjectiveSpeed = 0;
 
 byte motorMeasuredSpeed = 0;
 bool once = false;
@@ -62,16 +48,11 @@ byte kD = 1;
 byte prevNumber1 = 0;
 
 void setup() {
-  pinMode(MOTORDIRECTIONPIN, OUTPUT);
+
   pinMode(MOTORSPEEDPIN, OUTPUT);
-  pinMode(ENCODERA, INPUT);
-  pinMode(ENCODERB, INPUT);
-  pinMode(LEDGREEN, OUTPUT);
-  pinMode(LEDRED, OUTPUT);
-  pinMode(SWITCH, INPUT);
+
   Serial.begin(9600);
   Serial.println("Start");
-  digitalWrite(MOTORDIRECTIONPIN, 1); // Set motor to a rotation
   Wire.begin();
 }
 
@@ -87,7 +68,7 @@ void loop() {
     }
   } else {
     if (millis() > timerIntel + durationIntel) {
-      Serial.print("Objectif ");
+     /* Serial.print("Objectif ");
       Serial.println(motorObjectiveSpeed);
       Serial.print("Pulse  ");
       Serial.println(nbPulse);
@@ -97,7 +78,7 @@ void loop() {
       Serial.println(integral);
       Serial.print("value ");
       Serial.println(value);
-      timerIntel = millis();
+      timerIntel = millis();//*/
     }
 
     timerSensor = millis();
@@ -137,17 +118,14 @@ void regulationPID(byte objective, byte measuredValue) {
   }
   if (value < 0) {
     motorCurrentSpeed = 0;
-    digitalWrite(LEDRED, LOW);
-    digitalWrite(LEDGREEN, LOW);
+  
   } else {
     motorCurrentSpeed = value ;
   }
   if (motorCurrentSpeed == 255) {
-    digitalWrite(LEDGREEN, LOW);
-    digitalWrite(LEDRED, HIGH);
+
   } else {
-    digitalWrite(LEDGREEN, HIGH);
-    digitalWrite(LEDRED, LOW);
+
   }
   analogWrite(MOTORSPEEDPIN, motorCurrentSpeed);
 
--- Capteurs_tests/regulation_test/regulation_test.ino
+++ Capteurs_tests/regulation_test/regulation_test.ino
@@ -1,105 +1,99 @@
-#define Kp 0.02d
+#define Kp 0.002d
 #define Ki 0.002d
-#define Kd 0.01d
+#define Kd 0.0001d
 #define R 0.02d
 #define HOLE_NBR 50
-#define ARRAY_SIZE 2
+#define DURATION 20
+#define uS_IN_S 1000
+
 byte oldpulse1 = 0; // détection de flan
+
 double pps; //pulse par secondes
 double tps; // tours par secondes
-double avgSpeed = 0; // Moyenne de 10 vitesses calculées
-double Sumspeed = 0; //Somme temporaire des vitesses pour le calcul de avgSpeed
 double mesuredSpeed; // vitesse estimée en m/s
 double calculatedPWM = 0; // PWM à appliquer au moteur après régulation
 double error, derivative, integral, previous_error = 0; // erreur entre la vitesse voulue et la mesurée
+
 unsigned int pot = 0; // valeure du potentiomettre de test
 unsigned int desiredSpeed = 0; // vitesse voulue
+
 unsigned long currentTime[2]; // temps actuel permettant de calculer la vitesse
-unsigned long microsNow, microsPrev = 0;
+unsigned long millisNow, millisPrev = 0;
 
 unsigned long timerSensor = 0;
-unsigned long durationSensor = 20;
+unsigned long durationSensor = DURATION;
 
-unsigned long timerIntel = 0;
-unsigned long durationIntel = 1000;
+unsigned long timerPID = 0;
+unsigned long durationPID = DURATION;
 
 int nbPulse;
+
 bool flagPulse;
 
 void setup() {
-
+  nbPulse = 0;
+  flagPulse = false;
   Serial.begin(2000000); // pour ne pas trop ralentir le micro
   pinMode (2, INPUT);
-  analogWrite(5, 200);
+
 }
 
 void loop() {
 
   pot = analogRead(0);
-  desiredSpeed =   map(pot, 0, 1023, 0, 8);
+  desiredSpeed =   map(pot, 0, 1023, 0, 3);
   /* Serial.print("desiredSpeed ");
     Serial.println(desiredSpeed); //*/
 
 
-  for (byte j = 0; j < ARRAY_SIZE; j++) {
 
-    byte yolo = 0;
 
-    // détection de 2 pulses pour faire le calcul de vitesse intantanée (doit encore être modifié car bloquant)
-    //int timeout = millis();
-    if (timerSensor + durationSensor > micros()) {
-      if (digitalRead(2) == HIGH && flagPulse == false) {
-        flagPulse = true;
-        nbPulse++;
-      }
-      if (digitalRead(2) == false && flagPulse == true) {
-        flagPulse = false;
-      }
-    } else {
-      if (micros() > timerIntel + durationIntel) {
 
-        timerIntel = millis();
-      }
 
-      timerSensor = micros();
-      pps = (nbPulse/20)*1000000;
-
-      nbPulse = 0;
-    }
-
-    if (micros() > timerPID + durationPID) {
-      //analogWrite(MOTORSPEEDPIN, regulationPID(motorObjectiveSpeed, motorMeasuredSpeed));
-      regulationPID(motorObjectiveSpeed, motorMeasuredSpeed);
-      timerPID = micros();
-    }
-
-    
-    tps[j] = pps[j] / HOLE_NBR; //calcul du nombre de tours/secondes
-
-    mesuredSpeed[j] = 2 * PI * tps[j] * R; //calcul de la vitesse
-
+  // détection de 2 pulses pour faire le calcul de vitesse intantanée (doit encore être modifié car bloquant)
+  //int timeout = millis();
+  Serial.print("timerSensor ");
+  Serial.println(timerSensor);
+    Serial.print("durationSensor ");
+  Serial.println(durationSensor);
+    Serial.print("millis ");
+  Serial.println(millis()); //*/
+  if (timerSensor + durationSensor < millis()){
+    Serial.println("triggered");
+    pps = nbPulse*uS_IN_S/DURATION;
+    regulationPID();
+    timerSensor = millis();
+    nbPulse = 0;
   }
-
-  for (int i = 0; i < ARRAY_SIZE; i++) {
-
-    /* Serial.print(mesuredSpeed[i]); //les prints prennent trop de temps ils seront supprimé de la version définitive
-
-      Serial.println(" m/s"); //*/
-
-    Sumspeed += mesuredSpeed[i];
-    // Serial.println(Sumspeed);
-
+  else {
+    if (!flagPulse && !digitalRead(2)){
+      //Serial.println("added pulse");
+      flagPulse = true;
+      nbPulse++;
+    }
+    else if (digitalRead(2)) {
+      flagPulse = false;
+    }
   }
-  avgSpeed = Sumspeed / ARRAY_SIZE;
-  Sumspeed = 0;
+ 
+  tps = pps / HOLE_NBR; //calcul du nombre de tours/secondes
+
+  /*Serial.println(tps);
+  Serial.println(pps);//*/
+
+  mesuredSpeed = 2 * PI * tps * R; //calcul de la vitesse
 
   //partie de régulation PID
-  microsNow = millis(); //utile pour le Delta t pour l'integrale et la dérivée
 
+  
+}
+
+void regulationPID() {
+  millisNow = millis(); //utile pour le Delta t pour l'integrale et la dérivée
   if (desiredSpeed != 0) {
-    error = (desiredSpeed - avgSpeed) / desiredSpeed; //proportionnel
-    integral = integral + error * (microsNow - microsPrev); //integral
-    derivative = (error - previous_error) / (microsNow - microsPrev); //dérivatif
+    error = (desiredSpeed - mesuredSpeed) / desiredSpeed; //proportionnel
+    integral = integral + error * (millisNow - millisPrev); //integral
+    derivative = (error - previous_error) / (millisNow - millisPrev); //dérivatif
     calculatedPWM = constrain((Kp * error + Ki * integral + Kd * derivative) * 255, 0, 255); // somme du total
     previous_error = error;
   }
@@ -112,10 +106,10 @@ void loop() {
 
   /* Serial.print("calculatedPWM ");
     Serial.println(calculatedPWM);//*/
-  /* Serial.print("error ");
+   Serial.print("error ");
     Serial.println(error); //*/
-  /* Serial.print("avgSpeed ");
-    Serial.println(avgSpeed); //*/
+    Serial.print("mesuredSpeed ");
+    Serial.println(mesuredSpeed); //*/
 
-  microsPrev = millis(); //utile pour le Delta t pour l'integrale et la dérivée
+  millisPrev = millis(); //utile pour le Delta t pour l'integrale et la dérivée
 }
