27349729b8cd0a365ac3011f818c1cddb38bd412
PaulThonney <paul.thonney@cpnv.ch>
2019-06-06 15:23:42 +0200

    Update tracking_avant_2019.ino

--- Traquage/tracking_avant_2019/tracking_avant_2019.ino
+++ Traquage/tracking_avant_2019/tracking_avant_2019.ino
@@ -18,7 +18,7 @@ Servo servo; // donne un nom de Servomoteur
 
 #define PIN_SERVO 5
 
-int PositionX; //position de l'objet
+int posObj; //position de l'objet
 
 float posServo() {
   return mapfloat(servo.read(), 0, 180, -1, 1);
@@ -30,7 +30,7 @@ void setup() {
   Serial.begin(9600);
 
   servo.attach(PIN_SERVO); //atribue une pin avec PWM au servomoteur
-  servo.write(180); //met la tête droite
+  servo.write(90); //met la tête droite
   Serial.print("Starting...\n");
   pixy.init(); //démarre la caméra Pixy
 }
@@ -39,19 +39,17 @@ void loop() {
   uint16_t blocks; //nombre d'objets détecté par la Pixy
   if (pixy.getBlocks()) {
     blocks = pixy.getBlocks();
-    PositionX = pixy.blocks[0].x;
     //Serial.println(posObject(pixy.blocks[0].x));
     float posObj = posObject(pixy.blocks[0].x);
-    float diff = 5 * abs(abs(posObj) - abs(posServo()));
-    Serial.println(diff);
+    float posObjpositif = abs(posObj);
+    int incrementation = mapfloat(posObjpositif, 0 , 1 , 0,6);
+    //Serial.println(incrementation);   
     if (posObj < 0) {
-      servo.write(servo.read() - diff);
+      servo.write(servo.read() - incrementation);
     }
-    else if (posObj > 0) {
-      servo.write(servo.read() + diff);
+    else if (posObj > 0) { 
+      servo.write(servo.read() + incrementation);
     }
-
-
   }
 }
 
