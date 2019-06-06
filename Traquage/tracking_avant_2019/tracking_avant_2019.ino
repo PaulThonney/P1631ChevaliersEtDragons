f84c2a49699e865ed8246bc2355232c8fd02d79c
PaulThonney <paul.thonney@cpnv.ch>
2019-06-06 14:46:00 +0200

    Create tracking_avant_2019.ino

---
+++ Traquage/tracking_avant_2019/tracking_avant_2019.ino
@@ -0,0 +1,65 @@
+// Code de l'Arduino Nano gérant le traquage avant du Minotaure
+// Dany VALADO
+// 01.07.2018
+// Ce qu'il reste à ajouter dans le code :
+//  - mode pause
+//  - les capteurs laser (Lidar)
+//  - fluidifier la rotation du servo lors de la recherche de cible
+//  - contrôler que les proportions de la taille de l'objet détecté par la Pixy corresponde à la taille de référence
+//  - développer le code pour qu'il puisse prendre en compte plusieurs cibles
+
+#include <SPI.h> //comunication avec la Pixy
+#include <Wire.h> //I2C
+
+#include <Pixy.h>
+Pixy pixy; //donne un nom à la pixy
+
+#include <Servo.h>
+Servo servo; // donne un nom de Servomoteur
+
+//adresse I2C
+#define ADRESSE_PIXY1 12
+#define ADRESSE_INTELLIGENCE_CENTRALE 1
+#define ADRESSE_TRACKAGE 20
+
+#define PIN_SERVO 5
+
+int PositionX; //position de l'objet
+
+float posServo() {
+  return mapfloat(servo.read(), 0, 180, -1, 1);
+}
+float posObject(int pos) {
+  return mapfloat(pos, 0, 319, -1, 1);
+}
+void setup() {
+  Serial.begin(9600);
+
+  servo.attach(PIN_SERVO); //atribue une pin avec PWM au servomoteur
+  servo.write(180); //met la tête droite
+  Serial.print("Starting...\n");
+  pixy.init(); //démarre la caméra Pixy
+}
+
+void loop() {
+  uint16_t blocks; //nombre d'objets détecté par la Pixy
+  if (pixy.getBlocks()) {
+    blocks = pixy.getBlocks();
+    PositionX = pixy.blocks[0].x;
+    //Serial.println(posObject(pixy.blocks[0].x));
+    float posObj = posObject(pixy.blocks[0].x);
+    float diff = 5*abs(abs(posObj)-abs(posServo()));
+     Serial.println(diff);
+    if(posObj<0){
+      servo.write(servo.read()-diff);
+    }else if(posObj>0){
+      servo.write(servo.read()+diff);
+    }
+
+    
+  }
+}
+
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
