d051d20baf6620821c89acbd3bf37040d364af2e
PaulThonney <paul.thonney@cpnv.ch>
2019-06-06 16:44:17 +0200

    Ajout mesure distance

--- Traquage/tracking_avant_2019/tracking_avant_2019.ino
+++ Traquage/tracking_avant_2019/tracking_avant_2019.ino
@@ -15,9 +15,13 @@ Servo servo; // donne un nom de Servomoteur
 #define ADRESSE_PIXY1 12
 #define ADRESSE_INTELLIGENCE_CENTRALE 1
 #define ADRESSE_TRACKAGE 20
-
 #define PIN_SERVO 5
-
+#define TEMPS_CONTINUE_RECHERCHE 2000
+#define VITESSE_ROTATION_RECHERCHE 250
+#define TEMPS_ATTENTE_RECHERCHE 250
+bool lastSideObject;
+long unsigned lastTimeViewObject = 0;
+bool sensBalayage;
 int posObj; //position de l'objet
 
 float posServo() {
@@ -36,23 +40,64 @@ void setup() {
 }
 
 void loop() {
+  tracking();
+}
+
+unsigned int count=0;
+
+void tracking() {
   uint16_t blocks; //nombre d'objets détecté par la Pixy
   if (pixy.getBlocks()) {
     blocks = pixy.getBlocks();
-    //Serial.println(posObject(pixy.blocks[0].x));
+    lastTimeViewObject = millis();
     float posObj = posObject(pixy.blocks[0].x);
+   int width = (pixy.blocks[0].width);
+   int distance = map(width, 100,15,0,255);
+   if(distance<0){
+    distance = 0;
+   }
+    if(distance>255){
+    distance = 255;
+   }
+   Serial.println(distance);
     float posObjpositif = abs(posObj);
-    int incrementation = mapfloat(posObjpositif, 0 , 1 , 0,6);
-    //Serial.println(incrementation);   
+    int incrementation = mapfloat(posObjpositif, 0 , 1 , 0, 6);
     if (posObj < 0) {
       servo.write(servo.read() - incrementation);
+      lastSideObject = 1;
     }
-    else if (posObj > 0) { 
+    else if (posObj > 0) {
       servo.write(servo.read() + incrementation);
+      lastSideObject = 0;
+    }
+  }
+  else {
+    if (millis() < lastTimeViewObject + TEMPS_ATTENTE_RECHERCHE) {
+      return;
+    }
+    count++;
+    if(count%VITESSE_ROTATION_RECHERCHE!=0)return;
+    
+    if (millis() < lastTimeViewObject + TEMPS_CONTINUE_RECHERCHE) {
+      sensBalayage = lastSideObject;
+    }
+    byte pos = servo.read();
+    //Serial.println(String(sensBalayage) + " " + String(pos));
+    if (sensBalayage) {
+      servo.write(pos - 1);
+    }
+    else {
+      servo.write(pos + 1);
+    }
+
+    if (pos <= 5 ) {
+      sensBalayage = 0;
+    }
+    if (pos >= 175) {
+      sensBalayage = 1;
     }
   }
 }
-
 float mapfloat(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
